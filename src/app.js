c0d4207627bd27e6d2e23a62e3915be657350757
Ayanga Oluwamurewa <ayangaoluwamurewa@gmail.com>
2025-06-14 09:32:31 +0100

    Update

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ const isProduction = process.env.NODE_ENV === "production";
 // const dbURI = isProduction
 //   ? process.env.DB_URI
 //   : "mongodb://127.0.0.1:27017/checkuup";
-const port = Number(process.env.PORT);
+const port = Number(process.env.PORT) || 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -35,12 +35,7 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "assets")));
-app.use(formidable({}));
-
-app.use((req, res, next) => {
-  console.log("Incoming:", req.method, req.url);
-  next();
-});
+// app.use(formidable({}));
 
 app.get("/", (req, res) => {
   res.render("home");
